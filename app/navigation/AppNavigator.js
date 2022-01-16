508436faa005556e02f63deba066022ae13f625d
Ahmad mohammed <xpazzax11@gmail.com>
2022-01-16 08:26:26 +0300

    Add browse chalets throw Google map feature.

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  FontAwesome5,
+  FontAwesome,
+} from "@expo/vector-icons";
 import { View, Text } from "react-native";
-import HomeScreen from "../screens/HomeScreen";
-import MyChaletScreen from "../screens/myChaletScreen/MyChaletScreen";
 import AccountScreens from "../screens/AccountScreen";
+import SignUpScreen from "../screens/SignUpScreen";
 import MapScreen from "../screens/MapScreen";
 import Screen from "../components/Reusable/Screen";
 
 import HomeNavigator from "./HomeNavigator";
 import MyChaletNavigator from "./MyChaletNavigator";
+const allPosts = [
+  {
+    id: 1,
+    chaletName: "Al Morjan",
+    image: require("../photos/chalets1.png"),
+    price: 700,
+    rate: 4.5,
+    city: "Ryiadh",
+    description: "Nice chalet",
+    chaletLocation: { latitude: 24.77765, longitude: 45.544 },
+  },
+  {
+    id: 2,
+    chaletName: "Jm3yeh",
+    image: require("../photos/chalets2.jpg"),
+    price: 500,
+    rate: 3.5,
+    city: "Ryiadh",
+    description: "Nice chalet",
+    chaletLocation: { latitude: 24.77765, longitude: 46.544 },
+  },
+
+  {
+    id: 3,
+    chaletName: "Kharj",
+    image: require("../photos/chalets3.jpg"),
+    price: 450,
+    rate: 4.0,
+    city: "Ryiadh",
+    description: "Nice chalet",
+    chaletLocation: { latitude: 25.77765, longitude: 40.544 },
+  },
+];
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
+  const [postsArray, setpostsArray] = useState([...allPosts]);
   return (
     <Screen>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
@@ -28,13 +65,14 @@ const AppNavigator = () => {
         ></Tab.Screen>
         <Tab.Screen
           name="Map"
-          component={MapScreen}
           options={{
             tabBarIcon: ({ color, size }) => (
               <FontAwesome5 name="map-marker-alt" color={color} size={size} />
             ),
           }}
-        ></Tab.Screen>
+        >
+          {() => <MapScreen posts={postsArray} title="Test map " />}
+        </Tab.Screen>
         <Tab.Screen
           name="My chalets"
           component={MyChaletNavigator}
@@ -49,6 +87,15 @@ const AppNavigator = () => {
           }}
         ></Tab.Screen>
         <Tab.Screen
+          name="Sign up"
+          component={SignUpScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="sign-in" color={color} size={size} />
+            ),
+          }}
+        ></Tab.Screen>
+        {/* <Tab.Screen
           name="Account"
           component={AccountScreens}
           options={{
@@ -60,7 +107,7 @@ const AppNavigator = () => {
               />
             ),
           }}
-        ></Tab.Screen>
+        ></Tab.Screen> */}
       </Tab.Navigator>
     </Screen>
   );
--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,12 +1,118 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { Image } from "react-native-elements";
+import * as Location from "expo-location";
 
-const MapScreen = () => {
+import MapView, { Marker, Callout, CalloutSubview } from "react-native-maps";
+let cordenates = [
+  { latitude: 37.77765, longitude: 40.544 },
+  { latitude: 38.77765, longitude: 40.544 },
+  { latitude: 39.77765, longitude: 40.544 },
+];
+
+const MapScreen = ({ posts, title }) => {
+  const [pin, setpin] = useState(require("../photos/map-pin-2.png"));
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [locationEnabeled, setlocationEnabeled] = useState(false);
+  const [selectedPost, setselectedPost] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      setlocationEnabeled(true);
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <>
+      {selectedPost && (
+        <View
+          style={[
+            styles.container,
+            {
+              backgroundColor: "blue",
+              width: 100,
+              height: 100,
+              position: "absolute",
+            },
+          ]}
+        ></View>
+      )}
+      {locationEnabeled ? (
+        <View style={styles.container}>
+          <MapView
+            onPress={(e) => {
+              e.preventDefault();
+              setselectedPost(null);
+            }}
+            style={styles.map}
+            showsUserLocation={true}
+            initialRegion={{
+              latitude: 23.8859,
+              longitude: 45.0792,
+              latitudeDelta: 5.9222,
+              longitudeDelta: 5.6421,
+            }}
+          >
+            {posts.map((item, index) => (
+              <View>
+                <Marker
+                  key={index}
+                  coordinate={{
+                    latitude: item.chaletLocation.latitude,
+                    longitude: item.chaletLocation.longitude,
+                  }}
+                >
+                  <Callout
+                    style={styles.post}
+                    onPress={() => {
+                      console.log("test");
+                    }}
+                  >
+                    <Image source={pin} width={100} height={100} />
+                    <Text>{item.chaletName}</Text>
+                    <Text>{item.price}</Text>
+                  </Callout>
+                </Marker>
+              </View>
+            ))}
+          </MapView>
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <Text>You must enable location permession</Text>
+        </View>
+      )}
+    </>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  post: {
+    padding: 10,
+  },
+});
 export default MapScreen;
